5be990bb7dfc0757ca2b38eda27e1742983c52cd
Matt Hwang <mwhwang182@gmail.com>
2024-12-24 18:41:34 -0500

    Implement type files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
-import java.util.HashSet;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -7,7 +8,11 @@ public class Main {
     private static final Set<String> BUILTINS = Set.of("exit", "echo", "type");
 
     public static void main(String[] args) throws Exception {
-        
+
+        String path = System.getenv("PATH");
+
+        String[] paths = path.split(":");
+
         while(true) {
             System.out.print("$ ");
 
@@ -29,9 +34,16 @@ public class Main {
                     String value = tokens.length > 1 ? tokens[1] : "";
                     if( BUILTINS.contains(value) ) {
                         System.out.println(value + " is a shell builtin");
-                    } else {
-                        System.out.println(value + ": not found");
+                        break;
                     }
+
+                    String file = findFile(paths, value);
+                    if (!file.isEmpty()) {
+                        System.out.println(value + " is " + file);
+                        break;
+                    }
+
+                    System.out.println(value + ": not found");
                     break;
                 case "echo":
                     System.out.println(tokens.length > 1 ? tokens[1] : "");
@@ -45,5 +57,20 @@ public class Main {
     private static void doDefault(String input) {
         String output = input + ": command not found";
         System.out.println(output);
+    }
+
+    private static String findFile(String[] paths, String filename) {
+
+        for(String path: paths) {
+            File folder = new File(path);
+            FilenameFilter filter = (dir, name) -> name.equals(filename);
+            File[] matches = folder.listFiles(filter);
+
+            if (matches != null && matches.length > 0) {
+                return matches[0].toString();
+            }
+
+        }
+        return "";
     }
 }
