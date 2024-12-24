e3fa4ee87f621f6f3f4b159cfd307659fb9c3917
Matt Hwang <mwhwang182@gmail.com>
2024-12-24 14:57:28 -0500

    Implement REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        while(true) {
+            System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-        String output = input + ": command not found";
-        System.out.println(output);
+            String output = input + ": command not found";
+            System.out.println(output);
+        }
     }
 }
