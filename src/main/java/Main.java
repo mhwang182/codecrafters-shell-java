c295a33056ce266006255074ee58838e8cef4d28
Matt Hwang <mwhwang182@gmail.com>
2024-12-26 13:29:32 -0500

    Implement run program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -37,29 +37,50 @@ public class Main {
                         break;
                     }
 
-                    String file = findFile(paths, value);
-                    if (!file.isEmpty()) {
+                    File file = findFile(paths, value);
+                    if (file != null) {
                         System.out.println(value + " is " + file);
                         break;
                     }
 
                     System.out.println(value + ": not found");
                     break;
+
                 case "echo":
                     System.out.println(tokens.length > 1 ? tokens[1] : "");
                     break;
+
                 default:
+                    File processFile = findFile(paths, tokens[0]);
+                    if(processFile != null) {
+                        runProcess(tokens, processFile);
+                        break;
+                    }
                     doDefault(input);
             }
         }
     }
 
+    private static void runProcess(String[] tokens, File processFile) throws IOException {
+
+        String args = tokens.length > 1 ? " " + tokens[1] : "";
+        ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", tokens[0] + args);
+        processBuilder.redirectErrorStream(true);
+        processBuilder.directory(new File(processFile.getParent()));
+
+        Process process = processBuilder.start();
+
+        String result = new String(process.getInputStream().readAllBytes());
+        System.out.println(result);
+    }
+
     private static void doDefault(String input) {
+
         String output = input + ": command not found";
         System.out.println(output);
     }
 
-    private static String findFile(String[] paths, String filename) {
+    private static File findFile(String[] paths, String filename) {
 
         for(String path: paths) {
             File folder = new File(path);
@@ -67,10 +88,10 @@ public class Main {
             File[] matches = folder.listFiles(filter);
 
             if (matches != null && matches.length > 0) {
-                return matches[0].toString();
+                return matches[0];
             }
 
         }
-        return "";
+        return null;
     }
 }
